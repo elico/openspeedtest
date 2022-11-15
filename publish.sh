343feb163ca1445ffd6855c5a64f01c0b318a676
root <root@docker-host-2.novalocal>
2022-11-15 03:56:58 +0000

    Revert "cleanedup, maybe too much"
    This reverts commit 508c1dd6dd67b7b47e64b6ecb14d61be818b8a6d.

--- publish.sh
+++ publish.sh
@@ -4,9 +4,8 @@ set -x
 
 name="openspeedtest"
 docker_username="$(cat username)"
-version="1.0.1"
 
 docker login
-docker buildx build -t "${docker_username}/${name}:${version}" --platform linux/amd64,linux/arm64,linux/arm/v7 --push .
+docker buildx build -t "${docker_username}/${name}:latest" --platform linux/amd64,linux/arm64,linux/arm/v7 --push .
 
 set +x
